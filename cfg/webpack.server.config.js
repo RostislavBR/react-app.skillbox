d9c5270ce171c61f56c031203329819e8ea41c72
Rostislav <rvssv@bk.ru>
2022-12-08 16:10:29 +0300

    added new change to the branch preparation css modules

--- cfg/webpack.server.config.js
+++ cfg/webpack.server.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 const NODE_ENV = process.env.NODE_ENV;
+const GLOBAL_CSS_REGEXP = /\.global\.css/;
 
 module.exports = {
     target: 'node',
@@ -35,7 +36,12 @@ module.exports = {
                         }
                     },
                 ],
+                exclude: GLOBAL_CSS_REGEXP,
             },
+            {
+                test: GLOBAL_CSS_REGEXP,
+                use: ['css-loader'],
+            }
         ],
     },
     optimization: {
