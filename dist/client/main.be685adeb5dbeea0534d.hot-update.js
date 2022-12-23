f9c543c0d53811a221808a8f0de843cc212901dd
Rostislav <rvssv@bk.ru>
2022-12-23 16:36:18 +0300

    added new change

---
+++ dist/client/main.be685adeb5dbeea0534d.hot-update.js
@@ -0,0 +1,23 @@
+webpackHotUpdate("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/messagecounter.css":
+false,
+
+/***/ "./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/MessageCounter.tsx":
+false,
+
+/***/ "./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/index.ts":
+/*!******************************************************************************!*\
+  !*** ./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/index.ts ***!
+  \******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\Users\\\\bresd\\\\Desktop\\\\react-app.skillbox\\\\src\\\\shared\\\\Main\\\\CardList\\\\Card\\\\CardControlBar\\\\MessageCounter\\\\index.ts'\");\n\n//# sourceURL=webpack:///./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/index.ts?");
+
+/***/ }),
+
+/***/ "./src/shared/Main/CardList/Card/CardControlBar/MessageCounter/messagecounter.css":
+false
+
+})
---
+++ src/shared/Main/CardList/Card/CardInfo/CardInfoTitle/CardInfoTitle.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './cardinfotitle.css';
+
+export function CardInfoTitle() {
+    return (
+        <h1 className={styles['card-info-title']}>Реализация намеченных плановых<br/>заданий</h1>
+    );
+}
